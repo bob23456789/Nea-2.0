2c8ac1e2c0edf0358802d43fc87768c4bf9b2a8d
Dylan Cocker <AQ222486@aquinas.ac.uk>
2024-01-17 15:58:34 +0000

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+namespace Nea_2._0
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        const int tilesize = 55;
+        Texture2D squareTexture;
+        Texture2D grassTexture;
+        Texture2D treesquaretexture;
+        Texture2D menuTexture;
+        private Texture2D buttonTexture;
+        private Rectangle buttonRectangle; // square which teh tecture will be put in 
+        double gamestate = 1.5;
+        string menuTitle = "War On Perliculum\n             Prime";
+        string Line = "";
+        private SpriteFont myfontyfont;
+        public int[,] tilemap =
+        {
+              {0, 0, 0, 0, 0,0, 0, 0, 0, 0,0,0,0,0,0},
+              {0, 0, 0, 0, 0,0, 0, 0, 0, 0,0,0,0,0,0},
+              {0, 0, 0, 0, 0,0, 0, 0, 0, 0,0,0,0,0,0},
+              {0, 0, 0, 0, 0,0, 0, 0, 0, 0,0,0,0,0,0},
+              {0, 0, 0, 0, 0,0, 0, 0, 0, 0,0,0,0,0,0},
+              {0, 0, 0, 0, 0,0, 0, 0, 0, 0,0,0,0,0,0},
+              {0, 0, 0, 0, 0,0, 0, 0, 0, 0,0,0,0,0,0},
+              {0, 0, 0, 0, 0,0, 0, 0, 0, 0,0,0,0,0,0},
+              {0, 0, 0, 0, 0,0, 0, 0, 0, 0,0,0,0,0,0}
+         };
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+           //int[,] tilemap = 
+           //{
+           //    {0, 0, 0, 0, 0,0, 0, 0, 0, 0},
+           //    {0, 0, 0, 0, 0,0, 0, 0, 0, 0},
+           //    {0, 0, 0, 0, 0,0, 0, 0, 0, 0},
+           //    {0, 0, 0, 0, 0,0, 0, 0, 0, 0},
+           //    {0, 0, 0, 0, 0,0, 0, 0, 0, 0},
+           //    {0, 0, 0, 0, 0,0, 0, 0, 0, 0},
+           //    {0, 0, 0, 0, 0,0, 0, 0, 0, 0}
+           //};
+                
+
+            
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            if (gamestate == 1.5)
+            {
+                grassTexture = Content.Load<Texture2D>("grass");//loads grass 
+                treesquaretexture = Content.Load<Texture2D>("tree");// loads tree tile
+            }
+            if (gamestate == 1)
+            {
+                squareTexture = Content.Load<Texture2D>("menuscreen");
+                myfontyfont = Content.Load<SpriteFont>("File");
+                buttonTexture = Content.Load<Texture2D>("playbutton");
+                // Set the initial position and size of the button
+                Vector2 position = new Vector2(Window.ClientBounds.Width / 2 - 100, Window.ClientBounds.Height / 2 + 20);
+
+                buttonRectangle = new Rectangle((int)position.X, (int)position.Y, buttonTexture.Width, buttonTexture.Height);
+            }
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+            MouseState mouseState = Mouse.GetState();
+            if (mouseState.LeftButton == ButtonState.Pressed && buttonRectangle.Contains(mouseState.Position))
+            {
+                gamestate = 1.5;
+                LoadContent();
+            }
+            
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);//creats bit where grpahics goes
+            int col = 0;
+            int row = 0;
+            if (gamestate == 1)// menu screen  // y = 550 x = 825 area = 453750 pixles 
+            {
+                _spriteBatch.Begin();
+                _spriteBatch.Draw(squareTexture, new Vector2(row, col), Color.White);
+                Vector2 textMiddlePoint = myfontyfont.MeasureString(menuTitle) / 2;
+                // Places text in center of the screen
+                Vector2 position = new Vector2(Window.ClientBounds.Width / 2, Window.ClientBounds.Height / 2 - 50);
+                _spriteBatch.DrawString(myfontyfont, menuTitle, position, Color.AntiqueWhite, 0, textMiddlePoint, 1.5f, SpriteEffects.None, 1.0f);
+                _spriteBatch.Draw(buttonTexture, buttonRectangle, Color.White);
+                _spriteBatch.End();
+            }
+
+            if (gamestate == 1.5)
+            {
+                _spriteBatch.Begin();// begins draws  in the srpites 
+                for (int y = 0; y < tilemap.GetLength(0); y++)
+                {
+                    
+                    row = 0;
+                    for (int x = 0; x < tilemap.GetLength(1); x++)
+                    {
+                        _spriteBatch.Draw(grassTexture, new Vector2(row, col), Color.White);
+                        row += 55;
+                    }
+                    col += 55;
+                }
+                _spriteBatch.End();
+
+
+                base.Draw(gameTime);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿
+using var game = new Nea_2._0.Game1();
+game.Run();
+class tank
+{
+    private int _x; // cords
+    public int X { get { return _x; } }
+    private int _y;// y postion 
+    public int Y { get { return _y; } }
+    private int _armour;// armour value of tank  
+    public int Armour { get { return _armour; } }
+    private int _acc;// accuracy of tank 
+    public int Acc { get { return _acc; } }
+    private int _speed;// accuracy of tank 
+    public int Speed { get { return _speed; } }
+    private int _penpower;
+    public int Penpower { get { return _penpower; } }
+    private bool _apshell;
+    public bool Apshell { get { return _apshell; } }
+    private int _range;// hwo far the gun can shoot
+    public int Range { get { return _range; } }
+    private int _movementactionpoints;// how many movement action it can do 
+    public int Movactpoints { get { return _movementactionpoints; } }
+    private bool _havefired;// how many movement action it can do 
+    public bool Havefired { get { return _havefired; } }
+    public bool[] Components = new bool[5];  // components 1= engine  2= tracks 3=gun 4=turret ring if all destroyed tank is destroyed  tracks can be repaired 
+    public bool[] Crewmembers = new bool[5]; // crew 1= driver 2= gunner 3= loader 4= commander commander can switch wiht any loader can switch with gunner
+    public tank(int x, int y, int armour, int acc, int speed, int penpower, bool apshell, int range, int movepoints, bool havefired)
+    {
+
+        _x = x;
+        _y = y;
+        _armour = armour;
+        _acc = acc;
+        _speed = speed;
+        _penpower = penpower;
+        _apshell = apshell;
+        _range = range;
+        _movementactionpoints = movepoints;
+        _havefired = havefired;
+    }
+
+}
+
+
