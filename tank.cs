9285f25083a61615f169a39842a61e8c6b7b8c18
Dylan Cocker <AQ222486@aquinas.ac.uk>
2024-01-26 22:11:02 +0000

    getting tank draw class sorted

--- tank.cs
+++ tank.cs
@@ -34,14 +34,20 @@ namespace Nea_2._0
         public bool Apshell { get { return _apshell; } }
         private int _range;// hwo far the gun can shoot
         public int Range { get { return _range; } }
+        private int _x;
+        public int X { get { return _x; } }
+        private int _y;
+        public int Y { get { return _y; } }
         private int _movementactionpoints;// how many movement action it can do 
         public int Movactpoints { get { return _movementactionpoints; } }
         private bool _havefired;// how many movement action it can do 
         public bool Havefired { get { return _havefired; } }
         public bool[] Components = new bool[5];  // components 1= engine  2= tracks 3=gun 4=turret ring if all destroyed tank is destroyed  tracks can be repaired 
         public bool[] Crewmembers = new bool[5]; // crew 1= driver 2= gunner 3= loader 4= commander commander can switch wiht any loader can switch with gunner
-        public tank(int armour, int acc, int speed, int penpower, bool apshell, int range, int movepoints, bool havefired, int type, bool player)
+        public tank(int x, int y ,int armour, int acc, int speed, int penpower, bool apshell, int range, int movepoints, bool havefired, int type, bool player)
         {
+            _x = x;
+            _y = y;
             _player = player;
             _type = type;
             Location = new Vector2(0, 0);
@@ -64,7 +70,7 @@ namespace Nea_2._0
                 }
                 else if (Player == false)
                 {
-                    Texture = Content.Load<Texture2D>(@"Light_redRF");
+                    Texture = Content.Load<Texture2D>(@"Light_red_LF");
                 }
 
             }
@@ -72,11 +78,11 @@ namespace Nea_2._0
             {
                 if (Player == true)
                 {
-                    Texture = Content.Load<Texture2D>(@"Light_blue_RF");
+                    Texture = Content.Load<Texture2D>(@"blue_medium_RF");
                 }
                 else if (Player == false)
                 {
-                    Texture = Content.Load<Texture2D>(@"Light_redRF");
+                    Texture = Content.Load<Texture2D>(@"red_medium_LF");
                 }
 
             }
@@ -84,18 +90,25 @@ namespace Nea_2._0
             {
                 if (Player == true)
                 {
-                    Texture = Content.Load<Texture2D>(@"Light_blue_RF");
+                    Texture = Content.Load<Texture2D>(@"blueheavy_RF");
                 }
                 else if (Player == false)
                 {
-                    Texture = Content.Load<Texture2D>(@"Light_redRF");
+                    Texture = Content.Load<Texture2D>(@"red_heavy_LF");
                 }
 
             }
         }
         public override void Draw(SpriteBatch spriteBatch)
         {
-
+            int _X = 0;
+            int _Y = 0;
+            if(Player == true && Type == 3) 
+            { 
+                
+                spriteBatch = spriteBatch.Draw(Texture, new Vector2(_X, _Y),Microsoft.Xna.Framework.Color.White); 
+            }
+            
         }
     }
 }
